b8bde0958ec66a3e69064004123bbfc8d569b36d
QC-Promer <yangqichen@promer.co>
2019-10-14 14:19:20 +0800

    modify

--- modules/v1/services/CastingpinUserService.php
+++ modules/v1/services/CastingpinUserService.php
@@ -14,21 +14,22 @@ class CastingpinUserService {
     public static function Blocked($type,$openId){
         switch ($type){
             case 1:
-               //stage_name 职业
-               //university 毕业院校
-               //stage_name 艺名
-               //style 风格
-               //speciality特长
-               $data =   CastingpinActor::find()->select(['stage_name','university','stage_name','style','speciality'])->asArray()->one();
-               return $data;
-            break;
-            case 2:
                 //industry 行业
                 //company 公司
                 //position 职位
                 //city 城市
-               $data = CastingpinArranger::find()->select(['industry,company,position,city'])->asArray()->one();
-               return $data;
+                $data = CastingpinArranger::find()->where(['open_id'=>$openId])->select(['industry,company,position,city'])->asArray()->one();
+                return $data;
+
+            break;
+            case 2:
+                //stage_name 职业
+                //university 毕业院校
+                //stage_name 艺名
+                //style 风格
+                //speciality特长
+                $data =   CastingpinActor::find()->where(['open_id'=>$openId])->select(['stage_name','university','stage_name','style','speciality'])->asArray()->one();
+                return $data;
 
 
             break;
