625a39e3ecbce71f5b4b4fccda7d9322bc4d450a
QC-Promer <yangqichen@promer.co>
2019-10-16 10:30:16 +0800

    modify

--- modules/v1/controllers/CastingpinactorController.php
+++ modules/v1/controllers/CastingpinactorController.php
@@ -102,6 +102,9 @@ class CastingpinactorController extends BaseController
                     case 2:
                     $actor = CastingpinActor::find()->where(['open_id'=>$this->openId])->select(['wechat','phone','corporation',
                     'email','occupation','woman','university','stage_name','city','birthday','height','weight','speciality','profile'])->asArray()->one();
+
+                    
+
                     return HttpCode::renderJSON($actor, 'ok', '200');
                     break;
                 }
---
+++ modules/v1/controllers/CastingpinconfigController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace mcastingpin\modules\v1\controllers;
+
+use mcastingpin\common\components\Redis;
+use mcastingpin\common\helps\HttpCode;
+use mcastingpin\modules\v1\models\CastingpinConfig;
+use mcastingpin\modules\v1\models\CastingpinVersion;
+use yii\web\Controller;
+
+/**
+ * CastingpinConfigController implements the CRUD actions for CastingpinConfig model.
+ */
+class CastingpinconfigController extends Controller
+{
+    public  $enableCsrfValidation=false;
+
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+    /*
+ * 检验当前版本号
+ */
+    public function actionVersion()
+    {
+        if ((\Yii::$app->request->isPost)) {
+            $version =  \Yii::$app->request->post('version');
+            $valid = CastingpinVersion::find()->select(['version'])->asArray()->one();
+            if ($version != $valid['version']){
+                return  HttpCode::jsonObj(1,'ok',200);  //更新
+            }else{
+                return  HttpCode::jsonObj(0,'ok',200); //未更新
+            }
+        }else{
+            return  HttpCode::jsonObj([],'请求方式出错','418');
+        }
+    }
+    /*
+     * 获取Redis 缓存
+   */
+
+    public function  actionMessage(){
+        if ((\Yii::$app->request->isPost)) {
+            $valid = CastingpinVersion::find()->select(['version','status'])->asArray()->one();
+
+            $cache_msg =  CastingpinConfig::find()->select(['code','name','sort','type','title'])->asArray()->all();
+
+            $list =  Redis::get('castingpin_list');
+            if (!$list){
+                $list = $cache_msg;
+                Redis::set('castingpin_list',$list);
+            }
+            return  HttpCode::renderJSON($list,'ok',200);
+        }else{
+            return  HttpCode::renderJSON([],'请求方式出错','418');
+        }
+    }
+
+
+
+}
