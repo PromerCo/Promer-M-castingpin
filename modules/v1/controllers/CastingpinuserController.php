df0b86c46898e64b8fa1aad663c29a41f8c3daf6
QC-Promer <yangqichen@promer.co>
2019-10-16 21:04:16 +0800

    modify

--- modules/v1/controllers/CastingpinuserController.php
+++ modules/v1/controllers/CastingpinuserController.php
@@ -103,6 +103,36 @@ class CastingpinuserController extends BaseController
             return  HttpCode::renderJSON([],'请求方式出错','418');
         }
     }
+    /*
+     * 切换角色
+     */
+    public function actionCutrole(){
+        if ((\Yii::$app->request->isPost)) {
+
+            $type = \Yii::$app->request->post('type');
+            if (empty($type)){
+                return  HttpCode::renderJSON([],'参数不存在','412');
+            }
+            try {
+            $transaction = \Yii::$app->db->beginTransaction(); //开启事务
+            $is_success =  CastingpinUser::updateAll(['capacity'=>$type,'update_time'=>date('Y-m-d H:i:s',time())],['open_id'=>$this->openId]);
+                if ($is_success){
+                    $transaction->commit();
+                    return   HttpCode::renderJSON(CastingpinUserService::Blocked($type,$this->openId),'ok','201') ; //返回对应角色数据
+                }else{
+                    return  HttpCode::renderJSON([],'更新失败','416');
+                }
+            }catch (\Exception $e) {
+                return  HttpCode::renderJSON([],$e->getMessage(),'412');
+            }
+        }else{
+            return  HttpCode::renderJSON([],'请求方式出错','418');
+        }
+
+
+
+
+    }
 
 
 
