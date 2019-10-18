36ea7e02c98c5fb00cb76479fd215106ef0d2996
QC-Promer <yangqichen@promer.co>
2019-10-18 17:07:06 +0800

    modify

--- modules/v1/controllers/CastingpinhomeController.php
+++ modules/v1/controllers/CastingpinhomeController.php
@@ -38,11 +38,11 @@ class CastingpinhomeController extends BaseController
          if (empty($arranger['id'])){
              return  HttpCode::renderJSON([],'参数不能为空','416');
          }else{
-          $data = CastingpinNotice::findBySql("SELECT castingpin_user.avatar_url,castingpin_notice.arranger_id,castingpin_notice.id,castingpin_notice.notice_id,castingpin_notice.title,
+          $data = CastingpinNotice::findBySql("SELECT castingpin_user.avatar_url,castingpin_notice.arranger_id,castingpin_notice.id,castingpin_notice.cast_id,castingpin_notice.title,
 castingpin_notice.occupation,castingpin_notice.age,castingpin_notice.speciality,castingpin_notice.convene,castingpin_notice.create_time
 FROM castingpin_notice 
 LEFT JOIN castingpin_arranger ON castingpin_notice.arranger_id = castingpin_arranger.id
-LEFT JOIN castingpin_user  ON castingpin_user.open_id = castingpin_arranger.open_id")->asArray()->all();
+LEFT JOIN castingpin_user  ON castingpin_user.open_id = castingpin_arranger.open_id order castingpin_notice.create_time desc")->asArray()->all();
              foreach ($data as $key=>$value){
                  $data[$key]['create_time'] = Common::time_tranx($value['create_time'],1);
              }
@@ -55,7 +55,7 @@ LEFT JOIN castingpin_user  ON castingpin_user.open_id = castingpin_arranger.open
     */
     public function actionDetails(){
         $notice_id = \Yii::$app->request->post('notice_id');
-        $data = CastingpinNotice::findBySql("SELECT castingpin_user.avatar_url,castingpin_user.nick_name,castingpin_arranger.position,castingpin_notice.arranger_id,castingpin_notice.id,castingpin_notice.notice_id,castingpin_notice.title,
+        $data = CastingpinNotice::findBySql("SELECT castingpin_user.avatar_url,castingpin_user.nick_name,castingpin_arranger.position,castingpin_notice.arranger_id,castingpin_notice.id,castingpin_notice.cast_id,castingpin_notice.title,
 castingpin_notice.occupation,castingpin_notice.age,castingpin_notice.speciality,castingpin_notice.convene,castingpin_notice.profile,castingpin_notice.create_time
 FROM castingpin_notice 
 LEFT JOIN castingpin_arranger ON castingpin_notice.arranger_id = castingpin_arranger.id
