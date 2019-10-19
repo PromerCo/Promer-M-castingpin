e1f975e5cf848f2327020483740eb629614bd5b5
QC-Promer <yangqichen@promer.co>
2019-10-19 18:34:16 +0800

    modify

--- modules/v1/controllers/CastingpinnoticeController.php
+++ modules/v1/controllers/CastingpinnoticeController.php
@@ -263,7 +263,7 @@ WHERE  castingpin_notice.id = "'.$notice_id.'" AND   castingpin_actor.open_id="'
              * 更新收藏
              */
 
-            $is_update = CastingpinPull::updateAll(['is_collect' => $collect, 'update_time' => date('Y-m-d H:i:s', time())], [
+            $is_update = CastingpinPull::updateAll(['is_collect' => 1, 'update_time' => date('Y-m-d H:i:s', time())], [
                 'actor_id' => $notice_id,
                 'notice_id'=>$actor_id['id']
             ]);
