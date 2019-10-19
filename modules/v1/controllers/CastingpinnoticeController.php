c2814690337786c7950701d54befbbcb0fd14616
QC-Promer <yangqichen@promer.co>
2019-10-19 14:19:53 +0800

    modify

--- modules/v1/controllers/CastingpinnoticeController.php
+++ modules/v1/controllers/CastingpinnoticeController.php
@@ -120,8 +120,6 @@ class CastingpinnoticeController extends BaseController
                     //假如用户填写资料
                     $is_pull =   CastingpinPull::find()->where(['notice_id'=>$notice_id,'actor_id'=>$means['id']])->asArray()->count(); //接单
 
-                    return  HttpCode::renderJSON([],$is_pull,'200');
-
                     $material =  CastingpinUser::find()->where(['open_id'=>$this->openId])->select(['capacity'])->asArray()->one();  //身份标识（0 未填写资料 1 HUB 2KOL
                     if ($material['capacity'] != 2){
                         return  HttpCode::renderJSON([],'您不是KOL身份','417');
