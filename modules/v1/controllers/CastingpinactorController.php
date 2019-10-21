0e1b56269c51b1a8893018e963b612ec15fa04f7
QC-Promer <yangqichen@promer.co>
2019-10-21 14:04:52 +0800

    modify

--- modules/v1/controllers/CastingpinactorController.php
+++ modules/v1/controllers/CastingpinactorController.php
@@ -116,7 +116,7 @@ class CastingpinactorController extends BaseController
                     //统筹
                     case 1:
                     $actor = CastingpinArranger::find()->where(['open_id'=>$this->openId])->select(['wechat','phone', 'email',
-                        'industry','corporation','position','city','profile',''])->asArray()->one();
+                        'industry','corporation','position','city','profile'])->asArray()->one();
                     return HttpCode::renderJSON($actor, 'ok', '200');
 
                     break;
