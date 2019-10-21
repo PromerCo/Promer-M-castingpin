08bb6cd6173d7cc5e02ce5c4afc8fa7838c94c7e
QC-Promer <yangqichen@promer.co>
2019-10-21 20:16:19 +0800

    modify

--- modules/v1/controllers/CastingpinactorController.php
+++ modules/v1/controllers/CastingpinactorController.php
@@ -158,7 +158,7 @@ class CastingpinactorController extends BaseController
             $cast_id = \Yii::$app->request->post('cast_id');
             $data =  CastingpinActor::find()->where(['id'=>$cast_id])->select(['height','stage_name','phone','cover_video','cover_img','profile'
                 ,'speciality','occupation','woman','id','open_id'])->asArray()->one();
-            
+
             return HttpCode::renderJSON($data, 'ok', '200');
         }else{
             return  HttpCode::renderJSON([],'请求方式出错','418');
@@ -171,9 +171,8 @@ class CastingpinactorController extends BaseController
     */
     public function actionFollow(){
         if ((\Yii::$app->request->isPost)) {
-            $open_id =  $this->openId;
-            $actor_id  =  CastingpinUser::find()->where(['open_id'=>$open_id])->select(['id'])->one()['id']; //统筹 ID
-            $arranger_id = \Yii::$app->request->post('arranger_id');//关注人ID
+
+            $arranger_id = \Yii::$app->request->post('arranger_id');//被关注人ID
 
             $status = \Yii::$app->request->post('status')??1;  //0未关注  1已关注
             if (empty($arranger_id)){
@@ -181,30 +180,30 @@ class CastingpinactorController extends BaseController
             }
             $transaction = \Yii::$app->db->beginTransaction();
             //查看是否关注过
-            $follow_status =   CastingpinCarefor::find()->where(['actor_id'=>$actor_id,'arranger_id'=>$arranger_id])->select(['status'])->asArray()->one();
+            $follow_status =   CastingpinCarefor::find()->where(['actor_id'=>$this->openId,'arranger_id'=>$arranger_id])->select(['status'])->asArray()->one();
             //查看网红关注总人数
-            $follow_number = CastingpinActor::find()->where(['id'=>$actor_id])->select(['follow_number'])->asArray()->one()['follow_number'];
+            $follow_number = CastingpinActor::find()->where(['open_id'=>$this->openId])->select(['follow_number'])->asArray()->one()['follow_number'];
             if (!$follow_status){
                 //没有关注过(插入)
                 $is_success  =   \Yii::$app->db->createCommand()->insert('castingpin_carefor', [
                     'status' => $status,
                     'arranger_id' => $arranger_id,
-                    'actor_id'=>$actor_id
+                    'actor_id'=>$this->openId
                 ])->execute();
                 if ($is_success){
-                    CastingpinActor::updateAll(['follow_number'=>$follow_number+1,'update_time'=>date('Y-m-d H:i:s',time())],['id'=>$actor_id]);
+                    CastingpinActor::updateAll(['follow_number'=>$follow_number+1,'update_time'=>date('Y-m-d H:i:s',time())],['open_id'=>$this->openId]);
                     $transaction->commit();
                     return  HttpCode::renderJSON($status,'create is success','201');
                 }else{
                     return  HttpCode::renderJSON([],'error','412');
                 }
             }else{
-                $cancel_follow =    CastingpinCarefor::updateAll(['status'=>$status,'update_time'=>date('Y-m-d H:i:s',time())],['actor_id'=>$actor_id,'arranger_id'=>$arranger_id]);
+                $cancel_follow =    CastingpinCarefor::updateAll(['status'=>$status,'update_time'=>date('Y-m-d H:i:s',time())],['actor_id'=>$this->openId,'arranger_id'=>$arranger_id]);
                 if ($cancel_follow){
                     if ($status == 1){
-                        CastingpinActor::updateAll(['follow_number'=>intval($follow_number)+1,'update_time'=>date('Y-m-d H:i:s',time())],['id'=>$actor_id]);
+                         CastingpinActor::updateAll(['follow_number'=>intval($follow_number)+1,'update_time'=>date('Y-m-d H:i:s',time())],['open_id'=>$this->openId]);
                     }else{
-                        CastingpinActor::updateAll(['follow_number'=>intval($follow_number)-1,'update_time'=>date('Y-m-d H:i:s',time())],['id'=>$actor_id]);
+                        CastingpinActor::updateAll(['follow_number'=>intval($follow_number)-1,'update_time'=>date('Y-m-d H:i:s',time())],['open_id'=>$this->openId]);
                     }
                     $transaction->commit();
                     return  HttpCode::renderJSON($status,'ok','201');
