17ba4f72e62c29041ce5c16811b1d544b9e31048
QC-Promer <yangqichen@promer.co>
2019-10-23 17:01:04 +0800

    modify

--- modules/v1/controllers/CastingpinnoticeController.php
+++ modules/v1/controllers/CastingpinnoticeController.php
@@ -118,7 +118,7 @@ class CastingpinnoticeController extends BaseController
                     $is_pull =   CastingpinPull::find()->where(['notice_id'=>$notice_id,'actor_id'=>$means['id']])->asArray()->count(); //接单
                     $material =  CastingpinUser::find()->where(['open_id'=>$this->openId])->select(['capacity'])->asArray()->one();  //身份标识（0 未填写资料 1 统筹 2 艺人
                     if ($material['capacity'] != 2){
-                        return  HttpCode::renderJSON([],'您不是统筹身份','417');
+                        return  HttpCode::renderJSON([],'您不是艺人身份','417');
                     }
                     if (!$is_pull){
                        $pull_inster =  \Yii::$app->db->createCommand()->insert('castingpin_pull', [
