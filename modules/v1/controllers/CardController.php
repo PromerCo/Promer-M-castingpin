a787cb05a3d4f0228295f2868937cabb8cf6138b
QC-Promer <yangqichen@promer.co>
2019-10-28 13:29:25 +0800

    modify

--- modules/v1/controllers/CardController.php
+++ modules/v1/controllers/CardController.php
@@ -39,12 +39,77 @@ class CardController extends  Controller
     $defaut_poster = $new_card+$qrcode_height;//  整体高度
     $img = imagecreatetruecolor($defaut_w,$defaut_poster);
     $color = imagecolorallocate($img,255, 255, 255);//  白色
-    print_r($color);
+    imagefill($img,0,0,$color);
+    ImagePng($img,'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0c78105b888ba61ec0eece2f713597cc/0e2442a7d933c8956c0e8eeadb1373f08202002a.jpg');
+     ImageDestroy($img);
+    $path_1 =  'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0c78105b888ba61ec0eece2f713597cc/0e2442a7d933c8956c0e8eeadb1373f08202002a.jpg';//  生成的透明图片url
+    $path_2 = $this->actionImageResize(file_get_contents($img_1), $defaut_w, $new_card);
+    $image_1 = imagecreatefrompng($path_1);
+    $image_2 = imagecreatefrompng($path_2);
+    imagecopymerge($image_1, $image_2, 0, 0, 0, 0, imagesx($image_2), imagesy($image_2), 100);
+
+    print_r($image_1);
+    die;
 
 
 
 
+    }
 
+    /*
+ * 生成缩略图
+ * $image 图片url
+ * $width 缩放宽度
+ * $height 缩放高度
+ * $num    缩放比例，为0不缩放，不为0忽略参数2、3的宽高
+ */
+    function actionImageResize($imagedata,$width,$height,$num=0){
+
+        // 获取图像信息
+        list($bigWidth, $bigHight, $bigType) = getimagesizefromstring($imagedata);
+
+        // 缩放比例
+        if ($num > 0) {
+            $width  = $bigWidth * $num;
+            $height = $bigHight * $num;
+        }
+
+        // 创建缩略图画板
+        $block = imagecreatetruecolor($width, $height);
+
+        // 启用混色模式
+        imagealphablending($block, false);
+
+        // 保存PNG alpha通道信息
+        imagesavealpha($block, true);
+
+        // 创建原图画板
+        $bigImg = imagecreatefromstring($imagedata);
+
+        // 缩放
+        imagecopyresampled($block, $bigImg, 0, 0, 0, 0, $width, $height, $bigWidth, $bigHight);
+
+        // 生成临时文件名
+        $tmpFilename = tempnam(sys_get_temp_dir(), 'image_');
+
+        // 保存
+        switch ($bigType) {
+            case 1: imagegif($block, $tmpFilename);
+                break;
+
+            case 2: imagejpeg($block, $tmpFilename);
+                break;
+
+            case 3: imagepng($block, $tmpFilename);
+                break;
+        }
+
+        // 销毁
+        imagedestroy($block);
+
+        $image = $tmpFilename;
+
+        return $image;
     }
 
 
