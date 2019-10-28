8a3772b1a1f4e8fc43dc206ff3a6745531c25707
QC-Promer <yangqichen@promer.co>
2019-10-28 14:09:10 +0800

    modify

--- modules/v1/controllers/CardController.php
+++ modules/v1/controllers/CardController.php
@@ -27,16 +27,19 @@ class CardController extends  Controller
      *  合成图片
     */
     public function actionCompose(){
-
+        header("Content-Type:image/jpg");
         $pic_list       = array(
         'https://ss1.baidu.com/-4o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a9e671b9a551f3dedcb2bf64a4eff0ec/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg',
         'https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=05b297ad39fa828bce239be3cd1e41cd/0eb30f2442a7d9337119f7dba74bd11372f001e0.jpg',
         'https://ss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0d33eab267224f4a4899751339f69044/b3b7d0a20cf431ad7427dfad4136acaf2fdd98a9.jpg',
         'https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0cc74ef9a3773912db268361c8188675/9922720e0cf3d7ca810f3732f81fbe096a63a9fd.jpg',
         'https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=ad628627aacc7cd9e52d32d909032104/32fa828ba61ea8d3fcd2e9ce9e0a304e241f5803.jpg',
-        'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=d985fb87d81b0ef473e89e5eedc551a1/b151f8198618367aa7f3cc7424738bd4b31ce525.jpg'
+        'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=d985fb87d81b0ef473e89e5eedc551a1/b151f8198618367aa7f3cc7424738bd4b31ce525.jpg',
+            'https://ss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0d33eab267224f4a4899751339f69044/b3b7d0a20cf431ad7427dfad4136acaf2fdd98a9.jpg',
+            'https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=0cc74ef9a3773912db268361c8188675/9922720e0cf3d7ca810f3732f81fbe096a63a9fd.jpg',
+            'https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=ad628627aacc7cd9e52d32d909032104/32fa828ba61ea8d3fcd2e9ce9e0a304e241f5803.jpg'
         );
-    $pic_list    = array_slice($pic_list, 0, 6); // 只操作前9个图片
+    $pic_list    = array_slice($pic_list, 0, 9); // 只操作前6个图片
     $bg_w    = 150; // 背景图片宽度
     $bg_h    = 150; // 背景图片高度
     $background = imagecreatetruecolor($bg_w,$bg_h); // 背景图片
@@ -95,6 +98,30 @@ class CardController extends  Controller
                 $lineArr    = array(4);
                 $line_x  = 5;
                 break;
+            case 7:
+                $start_x    = 53;   // 开始位置X
+                $start_y    = 5;    // 开始位置Y
+                $pic_w   = intval($bg_w/3) - 5; // 宽度
+                $pic_h   = intval($bg_h/3) - 5; // 高度
+                $lineArr    = array(2,5);
+                $line_x  = 5;
+                break;
+            case 8:
+                $start_x    = 30;   // 开始位置X
+                $start_y    = 5;    // 开始位置Y
+                $pic_w   = intval($bg_w/3) - 5; // 宽度
+                $pic_h   = intval($bg_h/3) - 5; // 高度
+                $lineArr    = array(3,6);
+                $line_x  = 5;
+                break;
+            case 9:
+                $start_x    = 5;    // 开始位置X
+                $start_y    = 5;    // 开始位置Y
+                $pic_w   = intval($bg_w/3) - 5; // 宽度
+                $pic_h   = intval($bg_h/3) - 5; // 高度
+                $lineArr    = array(4,7);
+                $line_x  = 5;
+                break;
         }
         foreach( $pic_list as $k=>$pic_path ) {
             $kk = $k + 1;
@@ -103,6 +130,9 @@ class CardController extends  Controller
                 $start_y = $start_y + $pic_h + $space_y;
             }
             $pathInfo = pathinfo($pic_path);
+            
+            print_r($pathInfo);
+
             switch (strtolower($pathInfo['extension'])) {
                 case 'jpg':
                 case 'jpeg':
@@ -119,73 +149,15 @@ class CardController extends  Controller
             }
         }
         $resource   = $imagecreatefromjpeg($pic_path);
-        imagecopyresized($background,$resource,$start_x,$start_y,0,0,$pic_w,$pic_h,imagesx($resource),imagesy($resource)); // 最后两个参数为原始图片宽度和高度，倒数两个参数为copy时的图片宽度和高度
+        imagecopyresized($background,$resource,$start_x,$start_y,0,0,$pic_w,$pic_h,imagesx($resource),imagesy($resource));
         $start_x    = $start_x + $pic_w + $space_x;
-        header("Content-Type:image/jpg");
 
         imagejpeg($background);
-        imagegif($background, "./hero_gam.png");
-
-
 
 
 }
 
-    /*
- * 生成缩略图
- * $image 图片url
- * $width 缩放宽度
- * $height 缩放高度
- * $num    缩放比例，为0不缩放，不为0忽略参数2、3的宽高
- */
-    function actionImageResize($imagedata,$width,$height,$num=0){
 
-        // 获取图像信息
-        list($bigWidth, $bigHight, $bigType) = getimagesizefromstring($imagedata);
-
-        // 缩放比例
-        if ($num > 0) {
-            $width  = $bigWidth * $num;
-            $height = $bigHight * $num;
-        }
-
-        // 创建缩略图画板
-        $block = imagecreatetruecolor($width, $height);
-
-        // 启用混色模式
-        imagealphablending($block, false);
-
-        // 保存PNG alpha通道信息
-        imagesavealpha($block, true);
-
-        // 创建原图画板
-        $bigImg = imagecreatefromstring($imagedata);
-
-        // 缩放
-        imagecopyresampled($block, $bigImg, 0, 0, 0, 0, $width, $height, $bigWidth, $bigHight);
-
-        // 生成临时文件名
-        $tmpFilename = tempnam(sys_get_temp_dir(), 'image_');
-
-        // 保存
-        switch ($bigType) {
-            case 1: imagegif($block, $tmpFilename);
-                break;
-
-            case 2: imagejpeg($block, $tmpFilename);
-                break;
-
-            case 3: imagepng($block, $tmpFilename);
-                break;
-        }
-
-        // 销毁
-        imagedestroy($block);
-
-        $image = $tmpFilename;
-
-        return $image;
-    }
 
 
 
