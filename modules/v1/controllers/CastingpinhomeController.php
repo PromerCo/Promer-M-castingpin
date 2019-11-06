72650688368b50ebb9785c93a6db681542d5f481
QC-Promer <yangqichen@promer.co>
2019-11-07 03:32:39 +0800

    modify

--- modules/v1/controllers/CastingpinhomeController.php
+++ modules/v1/controllers/CastingpinhomeController.php
@@ -43,7 +43,7 @@ class CastingpinhomeController extends Controller
         $type =  \Yii::$app->request->post('type')??100600;  //剧组ID
         $start_page = \Yii::$app->request->post('start_page')??0; //页数
              if($type == 100600 ){
-                 $data = CastingpinCast::findBySql("SELECT id,script,type,cover_img,city,theme,browse,arranger_id,debut_time FROM  castingpin_cast  order by debut_time desc limit $start_page,8")->asArray()->all();
+                 $data = CastingpinCast::findBySql("SELECT id,script,type,cover_img,city,theme,browse,arranger_id,debut_time,create_time FROM  castingpin_cast  order by debut_time desc limit $start_page,8")->asArray()->all();
              }else{
                  $arranger_id =    CastingpinCast::find()->where(['type'=>$type])->select(['id'])->asArray()->all(); //剧组ID
 
@@ -59,9 +59,9 @@ LEFT JOIN castingpin_user  ON castingpin_user.open_id = castingpin_arranger.open
                      $data = [];
                  }
              }
-
+            //debut_time
              foreach ($data as $key=>$value){
-                 $data[$key]['debut_time'] = Common::time_tranx($value['debut_time'],0);
+                 $data[$key]['create_time'] = Common::time_tranx($value['create_time'],0);
              }
           return  HttpCode::renderJSON($data,'ok','201');
 
